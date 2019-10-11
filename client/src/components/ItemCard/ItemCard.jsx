a1e973242cc121abeaf3d9774d2dbb17d21c521c
ebru <ebky14@gmail.com>
2019-10-11 19:10:58 +0300

    item card design refactored

--- client/src/components/ItemCard/ItemCard.jsx
+++ client/src/components/ItemCard/ItemCard.jsx
@@ -6,22 +6,21 @@ import './ItemCard.scss';
 
 const ItemCard = ({ itemTitle }) => (
     <li className='item-card'>
-        <div className='item-title'>{itemTitle}</div>
-        <div className='item-icons'>
-            <CustomButton
-                className='check-button'
-                icon='check'
-                size='large'
-                shape='circle'
-                style={{ color: '#444', fontSize: '24px', border: 'None' }}
-            />
-            <CustomButton
-                className='delete-button'
-                icon='delete'
-                shape='circle'
-                style={{ color: '#aaa', fontSize: '17px', border: 'None' }}
-            />
+        <CustomButton
+            className='check-button'
+            icon='check'
+            shape='circle'
+            style={{ color: '#444', borderSize: '1px'}}
+        />
+        <div className='item-title'>
+            {itemTitle}
         </div>
+        <CustomButton
+            className='delete-button'
+            icon='delete'
+            shape='circle'
+            style={{ color: '#aaa', fontSize: '17px', border: 'None' }}
+        />
     </li>
 );
 
