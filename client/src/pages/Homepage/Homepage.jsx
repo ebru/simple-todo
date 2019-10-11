339d515eb0b50b90c0d7b6b6d3dd44c85c0a3ece
ebru <ebky14@gmail.com>
2019-10-11 15:55:50 +0300

    max width and padding improvements

--- client/src/pages/Homepage/Homepage.jsx
+++ client/src/pages/Homepage/Homepage.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import Header from './../../components/Header/Header';
 import TodoList from './../../components/TodoList/TodoList';
 
+import './Homepage.scss';
+
 const Homepage = () => (
     <div className='homepage-container'>
         <Header />
