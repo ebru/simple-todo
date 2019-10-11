2f77542ad3a53ce005a31316ae1e826d5a856240
ebru <ebky14@gmail.com>
2019-10-11 21:31:36 +0300

    delete item functionality is added

--- client/src/redux/todo/todo.reducer.js
+++ client/src/redux/todo/todo.reducer.js
@@ -1,29 +1,45 @@
 import todoActionTypes from './todo.types';
 
+import { deleteItem } from './todo.utils';
+
 const INITIAL_STATE = {
-    items: [],
-    errorMessage: undefined
+  items: [],
+  errorMessage: undefined
 };
 
 const todoReducer = (state = INITIAL_STATE, action) => {
-    switch (action.type) {
-      case todoActionTypes.FETCH_ITEMS_START:
-        return {
-          ...state
-        };
-      case todoActionTypes.FETCH_ITEMS_SUCCESS:
-        return {
-          ...state,
-          items: action.payload
-        };
-      case todoActionTypes.FETCH_ITEMS_FAILURE:
-        return {
-          ...state,
-          errorMessage: action.payload
-        };
-      default:
-        return state;
-    }
+  switch (action.type) {
+    case todoActionTypes.FETCH_ITEMS_START:
+      return {
+        ...state
+      };
+    case todoActionTypes.FETCH_ITEMS_SUCCESS:
+      return {
+        ...state,
+        items: action.payload
+      };
+    case todoActionTypes.FETCH_ITEMS_FAILURE:
+      return {
+        ...state,
+        errorMessage: action.payload
+      };
+    case todoActionTypes.DELETE_ITEM_START:
+      return {
+        ...state
+      };
+    case todoActionTypes.DELETE_ITEM_SUCCESS:
+      return {
+        ...state,
+        items: deleteItem(state.items, action.payload)
+      };
+    case todoActionTypes.DELETE_ITEM_FAILURE:
+      return {
+        ...state,
+        errorMessage: action.payload
+      };
+    default:
+      return state;
+  }
 };
-  
+
 export default todoReducer;
